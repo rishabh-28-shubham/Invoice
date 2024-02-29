1663860875c209a9d23c25fd774bad2727c4fc49
Rishabh Vishwakarma <ectopdash@gmail.com>
2024-02-29 22:48:18 +0530

    Assigning database details

--- invoice_venv/invoicee/invoicee/settings.py
+++ invoice_venv/invoicee/invoicee/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'invoicee.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default' : {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'invoice',
+        'USER': 'invoice_user',
+        'PASSWORD' : 'changeIt098',
+        'HOST' : 'localhost',
+        'PORT' : ''
     }
 }
 
