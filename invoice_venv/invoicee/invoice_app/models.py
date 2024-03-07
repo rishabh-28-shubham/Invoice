d2eed8b7d56de9505db347a0ba956487df82daf6
Rishabh Vishwakarma <ectopdash@gmail.com>
2024-03-07 23:07:27 +0530

    change's def (self) and also uplaoding Invoice Models

--- invoice_venv/invoicee/invoice_app/models.py
+++ invoice_venv/invoicee/invoice_app/models.py
@@ -73,7 +73,7 @@ class Product(models.Model):
     last_updated = models.DateTimeField(blank=True, null=True)
 
 
-    def __str__(self) -> str:
+    def __str__(self):
         return '{} {}'.format(self.title, self.uniqueId)
     
     def get_absolute_rul(self):
@@ -94,3 +94,59 @@ class Product(models.Model):
         super(Product, self).save(*args,**kwargs)
 
         
+
+#Invoice Models
+class Invoice(models.Model):
+    TERMS =[
+        ('14 days','14 days'),
+        ('30 days','30 days'),
+        ('60 days','60 days'),
+        ('90 days','90 days'),
+    ]
+
+    STATUS = [
+        ('CURRNET','CURRENT'),
+        ('OVERDUE','OVERDUE'),
+        ('PAID','PAID'),
+    ]
+
+
+    #basic fileds --> Invoice Model
+    title = models.CharField(null=True, blank=True, max_length=100)
+    number = models.CharField(null=True, blank=True, max_length=100)
+    dueDate = models.DateField(null=True, blank=True)
+    paymentTerms = models.CharField(choices=TERMS, default='14 days', max_length=100)
+    status = models.CharField(choices=STATUS,  default='CURRENT', max_length=100)
+    notes = models.TextField(null=True, blank=True)
+
+    #accessing clinet and product model
+    client = models.ForeignKey(Client, null=True, blank=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, blank=True, on_delete=models.SET_NULL)
+
+
+    #utility filelds
+    uniqueId = models.CharField(null=True, blank=True, max_length=100)
+    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+    date_created = models.DateTimeField(blank=True, null=True)
+
+
+
+    def __str__(self):
+        return '{} {}'.format(self.title, self.uniqueId)
+    
+    def get_absolute_rul(self):
+        return reverse('invoice-detail', kwargs={'slug': self.slug})
+    
+
+    def save(self, *args, **kwargs):
+        if self.date_created is None:
+            self.date_created = timezone.localtime(timezone.now())
+        if self.uniqueId is None:
+            self.uniqueId = str(uuid4()).split('-')[4]
+            self.slug = slugify('{} {}'.format(self.title, self.uniqueId))
+
+        
+        self.slug = slugify('{} {}'.format(self.title, self.uniqueId))
+        self.last_updated = timezone.localtime(timezone.now())
+
+        super(Invoice, self).save(*args,**kwargs)
