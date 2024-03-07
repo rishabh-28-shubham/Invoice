3564a56834b67a3b43a331c465ca7481cb77d2bd
Rishabh Vishwakarma <ectopdash@gmail.com>
2024-03-07 23:13:03 +0530

    uplaoding settings model

--- invoice_venv/invoicee/invoice_app/models.py
+++ invoice_venv/invoicee/invoice_app/models.py
@@ -150,3 +150,49 @@ class Invoice(models.Model):
         self.last_updated = timezone.localtime(timezone.now())
 
         super(Invoice, self).save(*args,**kwargs)
+
+
+
+#settings model
+class Settings(models.Models):
+
+    PROVINCES = [
+        ('Gauteng', 'Gauteng'),
+        ('Free State', 'Free State'),
+        ('Limpopo', 'Limpopo'),
+    ]
+
+    #basic fileds
+    clientName = models.CharField(null=True, blank=True, max_length=200)
+    addressLine1 = models.CharField(null=True, blank=True, max_length=200)
+    province = models.CharField(choices=PROVINCES, blank=True, max_length=100)
+    postalCode =  models.CharField(null=True, blank=True, max_length=10)
+    phoneNumber =  models.CharField(null=True, blank=True, max_length=100)
+    emailAddress =  models.CharField(null=True, blank=True, max_length=100)
+
+    # Utility fields
+    uniqueId = models.CharField(null=True, blank=True, max_length=100)
+    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+    date_created = models.DateTimeField(blank=True, null=True)
+    last_update = models.DateTimeField(blank=True, null=True)
+
+
+    def __str__(self):
+        return '{} {} {}'.format(self.clientName, self.province ,self.uniqueId)
+    
+
+    def get_absolute_url(self):
+        return reverse('client-detail', kwargs={'slug': self.slug})
+    
+
+    def save(self, *args, **kwargs):
+        if self.date_created is None:
+            self.date_created = timezone.localtime(timezone.now())
+        if self.uniqueId is None:
+            self.uniqueId = str(uuid4()).split('=')[4]
+            self.slug = slugify('{} {} {}'.format(self.clientName, self.province ,self.uniqueId))
+
+        self.slug = slugify('{} {} {}'.format(self.clientName, self.province ,self.uniqueId))
+        self.last_update = timezone.localtime(timezone.now())
+
+        super(Client, self).save(*args, **kwargs)
