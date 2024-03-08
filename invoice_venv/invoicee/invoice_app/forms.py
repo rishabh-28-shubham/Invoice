b812142bf90d55feb7b7c489c712464e4d1ed2d8
Rishabh Vishwakarma <ectopdash@gmail.com>
2024-03-08 13:08:42 +0530

    forms :: --> Client, Product and Settings, also for userlogin and DateInput

--- invoice_venv/invoicee/invoice_app/forms.py
+++ invoice_venv/invoicee/invoice_app/forms.py
@@ -3,3 +3,49 @@ from django.contrib.auth.models import User
 from django.forms import widgets
 from .models import Client, Product, Invoice, Settings
 import json
+
+
+#form for DateInput
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+# form for UserLogin
+class UserLoginForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username','password']
+
+
+# form for Client
+class ClientForm(forms.ModelForm):
+    class Meta:
+        model = Client
+        fields = ['clientName','addressLine1', 'province', 'postalCode', 'phoneNumber', 'emailAddress']
+
+
+# form for Product
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['title','discription','quantity','price','currency']
+
+
+
+# form for Invoice
+class InvoiceForm(forms.ModelForm):
+    dueDate = forms.DateField(
+        required = True,
+        label = 'Invoice Due',
+        widget = DateInput(attrs={'class': 'form-control'}),
+    )
+
+    class Meta:
+        model = Invoice
+        fields = ['title','number','dueDate','paymentTerms','status','notes']
+
+# form for Settings
+class SettingsForm(forms.ModelForm):
+    class Meta:
+        model = Settings
+        fields = ['clientName','addressLine1','province','postalCode','phoneNumber','emailAddress']
