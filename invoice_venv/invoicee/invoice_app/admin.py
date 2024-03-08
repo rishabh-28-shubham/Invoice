d315e172dcb1d0ee83bd0da76b177718b00d37da
Rishabh Vishwakarma <ectopdash@gmail.com>
2024-03-08 11:33:45 +0530

    Register Settings, Product and Invoice Models

--- invoice_venv/invoicee/invoice_app/admin.py
+++ invoice_venv/invoicee/invoice_app/admin.py
@@ -5,3 +5,8 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Client)
+
+#Register Settings, Product and Invoice Models
+admin.site.register(Product)
+admin.site.register(Invoice)
+admin.site.register(Settings)
